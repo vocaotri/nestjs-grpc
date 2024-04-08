a39a92f91c34b387970e2a6828bc1f6bcf00c39c
BM Trisma <trisma@brickmate.vn>
2024-04-08 21:18:34 +0700

    first commit

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,41 @@
+import {
+  CreateUserDto,
+  UpdateUserDto,
+  FindOneUserDto,
+  UsersServiceController,
+  UsersServiceControllerMethods,
+  PaginationDto,
+} from '@app/common';
+import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Observable } from 'rxjs';
+
+@Controller()
+@UsersServiceControllerMethods()
+export class UsersController implements UsersServiceController {
+  constructor(private readonly usersService: UsersService) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  findAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  findOneUser(findOneUserDto: FindOneUserDto) {
+    return this.usersService.findOne(findOneUserDto.id);
+  }
+
+  updateUser(updateUserDto: UpdateUserDto) {
+    return this.usersService.update(updateUserDto.id, updateUserDto);
+  }
+
+  removeUser(findOneUserDto: FindOneUserDto) {
+    return this.usersService.remove(findOneUserDto.id);
+  }
+
+  queryUsers(paginationDtoStream: Observable<PaginationDto>) {
+    return this.usersService.queryUsers(paginationDtoStream);
+  }
+}
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,84 @@
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  User,
+  CreateUserDto,
+  UpdateUserDto,
+  Users,
+  PaginationDto,
+} from '@app/common';
+import { randomUUID } from 'crypto';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private readonly users: User[] = [];
+
+  onModuleInit() {
+    for (let i = 0; i < 100; i++) {
+      this.create({
+        username: `user${i}`,
+        password: `password${i}`,
+        age: i,
+      });
+    }
+  }
+
+  create(createUserDto: CreateUserDto): User {
+    const user: User = {
+      ...createUserDto,
+      subscribed: false,
+      socialMedia: {},
+      id: randomUUID(),
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  findAll(): Users {
+    return {
+      users: this.users,
+    };
+  }
+
+  findOne(id: string): User {
+    return this.users.find((user) => user.id === id);
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) throw new NotFoundException('User not found');
+    let user = this.users[userIndex];
+    user = {
+      ...user,
+      ...updateUserDto,
+    };
+    this.users[userIndex] = user;
+    return user;
+  }
+
+  remove(id: string): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) throw new NotFoundException('User not found');
+    const user = this.users[userIndex];
+    this.users.splice(userIndex, 1);
+    return user;
+  }
+
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    const subject = new Subject<Users>();
+    const onNext = (paginationDto: PaginationDto) => {
+      const start = paginationDto.page * paginationDto.limit;
+      subject.next({
+        users: this.users.slice(start, start + paginationDto.limit),
+      });
+    };
+    const onComplete = () => subject.complete();
+    paginationDtoStream.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+    return subject.asObservable();
+  }
+}
